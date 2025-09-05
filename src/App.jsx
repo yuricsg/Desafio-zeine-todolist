17e170942ad7fdda930db8234f87e98b0b9bce3b
yuricsg <yuriicardo004@gmail.com>
2025-09-04 21:38:30 -0300

    feat: create basic todo list structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Extrair dados da tabela",
+      category: "Profissional",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Correr ao entardecer",
+      category: "Vida pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 1,
+      text: "Estudar React",
+      category: "Acadêmico",
+      isCompleted: false,
+    }
+  ])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div className='app'>
+    <h1>To-Do List:</h1>
+    <div className="todo-list">
+      {
+        todos.map((todo) => (
+            <div className="todo">
+              <div className="content">
+                <p>{todo.text}</p>
+                <p className="category">({todo.category})</p>
+              </div>
+              <div>
+                <button>Concluir</button>
+                <button>Deletar</button>
+              </div>
+            </div>
+        ))
+      }
+    </div>
+  </div>
 }
 
 export default App
